1750f301e3a68107f46f0f630594ea9d51116fdf
Alfred <alfred.ahonen@gmail.com>
2023-02-14 17:20:06 +0200

    etsi osoite

--- App.js
+++ App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Button, TextInput, Alert } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [region, setRegion] = useState({
+    latitude: 60.2,
+    longitude: 24.934,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221
+  });
+  const [place, setPlace] = useState('');
+  const [data, setData] = useState({
+    latitude: 60.2013,
+    longitude: 24.934
+  });
+
+  const search = () => {
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=hoZvdAWRuIgL9ynTqcTAQVblZcAmArHC&location=${place}`)
+    .then(response => response.json())
+    .then(data => {
+      setRegion({
+        latitude: data.results[0].locations[0].latLng.lat, 
+        longitude: data.results[0].locations[0].latLng.lng,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221
+      });
+      setData({
+        latitude: data.results[0].locations[0].latLng.lat, 
+        longitude: data.results[0].locations[0].latLng.lng
+      })
+    })
+    .catch(error => Alert.alert('Error', 'Invalid input'))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView
+        style={{ width: '100%', height: '75%'}}
+        region={region}
+      >
+        <Marker
+          pinColor='yellow'
+          coordinate={data}
+        />
+      </MapView>
+      <TextInput style={{width: 200, height: 40, borderWidth: 1, margin: 6}} onChangeText={text => setPlace(text)} value={place} />
+      <Button onPress={search} title='Search Address' />
     </View>
   );
 }
